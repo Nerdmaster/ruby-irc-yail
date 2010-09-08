bb601ba1ac3778c2f7d117b98a3c9807e5d730d9
Nerdmaster <brody76@gmail.com>
2010-09-08 19:16:28 -0400

    Add deprecation warning to IRCBot.rb

--- lib/net/yail/IRCBot.rb
+++ lib/net/yail/IRCBot.rb
@@ -1,3 +1,6 @@
+# This is a deprecated file!
+warn '[DEPRECATED] Requiring "net/yail/IRCBot" is deprecated!  Use "net/yail/irc_bot" instead.'
+
 # Wrapper for irc_bot for backward-compatibility
 require 'rubygems'
 require 'net/yail'
