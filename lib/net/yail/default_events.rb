73aaa13f3b61d4fc3abf41148efc40a223d4007a
Nerdmaster <brody76@gmail.com>
2010-11-15 19:58:10 -0500

    Fix default events
    * Uses event object in all methods
    * Reorganizes code to get report-only methods grouped together

--- lib/net/yail/default_events.rb
+++ lib/net/yail/default_events.rb
@@ -6,59 +6,64 @@ module IRCEvents
 module Defaults
   private
 
-  def r_msg(fullactor, actor, target, text)
-    report "{#{target}} <#{actor}> #{text}"
+  def r_msg(event)
+    report "{%s} <%s> %s" % [event.target || event.channel, event.nick, event.text]
   end
 
-  def r_act(fullactor, actor, target, text)
-    report "{#{target}} * #{actor} #{text}"
+  def r_act(event)
+    report "{%s} * %s %s" % [event.target || event.channel, event.nick, event.text]
   end
 
-  def r_notice(fullactor, actor, target, text)
-    report "{#{target}} -#{actor}- #{text}"
+  def r_notice(event)
+    report "{%s} -%s- %s" % [event.target || event.channel, event.nick, event.text]
   end
 
-  def r_ctcp(fullactor, actor, target, text)
-    report "{#{target}} [#{actor} #{text}]"
+  def r_ctcp(event)
+    report "{%s} [%s %s]" % [event.target || event.channel, event.nick, event.text]
   end
 
-  def r_ctcpreply(fullactor, actor, target, text)
-    report "{#{target}} [Reply: #{actor} #{text}]"
+  def r_ctcpreply(event)
+    report "{%s} [Reply: %s %s]" % [event.target || event.channel, event.nick, event.text]
   end
 
-  def r_mode(fullactor, actor, target, modes, objects)
-    report "{#{target}} #{actor} sets mode #{modes} #{objects}"
+  def r_mode(event)
+    report "{%s} %s sets mode %s %s" % [event.channel, event.nick, event.text, event.targets.join(' ')]
   end
 
-  def r_join(fullactor, actor, target)
-    report "{#{target}} #{actor} joins"
+  def r_join(event)
+    report "{#{event.channel}} #{event.nick} joins"
   end
 
-  def r_part(fullactor, actor, target, text)
-    report "{#{target}} #{actor} parts (#{text})"
+  def r_part(event)
+    report "{#{event.channel}} #{event.nick} parts (#{event.text})"
   end
 
-  def r_kick(fullactor, actor, target, object, text)
-    report "{#{target}} #{actor} kicked #{object} (#{text})"
+  def r_kick(event)
+    report "{#{event.channel}} #{event.nick} kicked #{event.target} (#{event.text})"
   end
 
-  def r_quit(fullactor, actor, text)
-    report "#{actor} quit (#{text})"
+  def r_quit(event)
+    report "#{event.nick} quit (#{event.text})"
+  end
+
+  # Incoming invitation
+  def r_invite(event)
+    report "[#{event.nick}] INVITE to #{event.target}"
   end
 
   # Reports nick change unless nickname is us - we check nickname here since
   # the magic method changes @me to the new nickname.
-  def r_nick(fullactor, actor, nickname)
-    report "#{actor} changed nick to #{nickname}" unless nickname == @me
+  def r_nick(event)
+    report "#{event.nick} changed nick to #{event.text}" unless nickname == @me
   end
 
-  def r_bannedfromchan(text, args)
-    text =~ /^(\S*) :Cannot join channel/
+  def r_bannedfromchan(event)
+    event.text =~ /^(\S*) :Cannot join channel/
     report "Banned from channel #{$1}"
   end
 
-  def r_badchannelkey(text, args)
-    text =~ /^(\S*) :Cannot join channel/
+  def r_badchannelkey(event)
+    event.text =~ /^(\S*) :Cannot join channel/
     report "Bad channel key (password) for #{$1}"
   end
 
@@ -66,6 +71,45 @@ module Defaults
     report "*** Logged in as #{@me}. ***"
   end
 
+  # Channel URL
+  def r_channelurl(event)
+    event.text =~ /^(\S+) :?(.+)$/
+    report "{#{$1}} URL is #{$2}"
+  end
+
+  # Channel topic
+  def r_topic(event)
+    event.text =~ /^(\S+) :?(.+)$/
+    report "{#{$1}} Topic is: #{$2}"
+  end
+
+  # Channel topic setter
+  def r_topicinfo(event)
+    event.text =~ /^(\S+) (\S+) (\d+)$/
+    report "{#{$1}} Topic set by #{$2} on #{Time.at($3.to_i).asctime}"
+  end
+
+  # End of names
+  def r_endofnames(event)
+    event.text =~ /^(\S+)/
+    report "{#{$1}} Nickname list complete"
+  end
+
+  # MOTD line
+  def r_motd(event)
+    event.text =~ /^:?(.+)$/
+    report "*MOTD* #{$1}"
+  end
+
+  # Beginning of MOTD
+  def r_motdstart(event)
+    event.text =~ /^:?(.+)$/
+    report "*MOTD* #{$1}"
+  end
+
+  # End of MOTD
+  def r_endofmotd(event)
+    report "*MOTD* End of MOTD"
   end
 
   # Nickname change failed: already in use.  This needs a rewrite to at
@@ -77,8 +121,8 @@ module Defaults
   #
   # TODO: This should really not even be here.  Client should have full control over whether or not
   # they want this.  Base IRC bot class should have this, but not the core YAIL lib.
-  def _nicknameinuse(text, args)
-    text =~ /^(\S+)/
+  def _nicknameinuse(event)
+    event.text =~ /^(\S+)/
     report "Nickname #{$1} is already in use."
 
     if (!@registered)
@@ -97,29 +141,11 @@ module Defaults
     end
   end
 
-  # Channel URL
-  def r_channelurl(text, args)
-    text =~ /^(\S+) :?(.+)$/
-    report "{#{$1}} URL is #{$2}"
-  end
-
-  # Channel topic
-  def r_topic(text, args)
-    text =~ /^(\S+) :?(.+)$/
-    report "{#{$1}} Topic is: #{$2}"
-  end
-
-  # Channel topic setter
-  def r_topicinfo(text, args)
-    text =~ /^(\S+) (\S+) (\d+)$/
-    report "{#{$1}} Topic set by #{$2} on #{Time.at($3.to_i).asctime}"
-  end
-
   # Names line
   #
   # TODO: Either store this data silently or ditch this code - this verbosity doesn't belong in a core lib
-  def _namreply(text, args)
-    text =~ /^(@|\*|=) (\S+) :?(.+)$/
+  def _namreply(event)
+    event.text =~ /^(@|\*|=) (\S+) :?(.+)$/
     channeltype = {'@' => 'Secret', '*' => 'Private', '=' => 'Normal'}[$1]
     report "{#{$2}} #{channeltype} channel nickname list: #{$3}"
     @nicklist = $3.split(' ')
@@ -127,41 +153,13 @@ module Defaults
     report "First nick: #{@nicklist[0]}"
   end
 
-  # End of names
-  def r_endofnames(text, args)
-    text =~ /^(\S+)/
-    report "{#{$1}} Nickname list complete"
-  end
-
-  # MOTD line
-  def r_motd(text, args)
-    text =~ /^:?(.+)$/
-    report "*MOTD* #{$1}"
-  end
-
-  # Beginning of MOTD
-  def r_motdstart(text, args)
-    text =~ /^:?(.+)$/
-    report "*MOTD* #{$1}"
-  end
-
-  # End of MOTD
-  def r_endofmotd(text, args)
-    report "*MOTD* End of MOTD"
-  end
-
   # We dun connected to a server!  Just sends password (if one is set) and
   # user/nick.  This isn't quite "essential" to a working IRC app, but this data
   # *must* be sent at some point, so be careful before skipping this handler.
-  def out_begin_connection(username, address, realname)
+  def out_begin_connection(event)
     pass(@password) if @password
-    user(username, '0.0.0.0', address, realname)
+    user(event.username, '0.0.0.0', event.address, event.realname)
     nick(@nicknames[0])
-  end
-
-  # Incoming invitation
-  def r_invite(fullactor, actor, target)
-    report "[#{actor}] INVITE to #{target}"
   end
 
 end
