33f42b98cdaa9bf9b368140722007a5c10033339
Nerdmaster <brody76@gmail.com>
2010-12-08 17:34:17 -0500

    Centralize YAIL test setup and capture notice events

--- tests/tc_yail.rb
+++ tests/tc_yail.rb
@@ -6,16 +6,20 @@ require 'test/unit'
 # This test suite is built as an attempt to validate basic functionality in YAIL.  Due to the
 # threading of the library, things are going to be... weird.  Good luck, me.
 class MessageParserEventTest < Test::Unit::TestCase
-  # Log in to fake server, give name, choose nick, join a channel, get kicked, quit
-  def test_simple
+  def setup
     @msg = Hash.new(0)
-    yail = Net::YAIL.new(
+    @yail = Net::YAIL.new(
       :io => MockIRC.new, :silent => true, :address => 'fake-irc.nerdbucket.com',
       :nicknames => ['Bot'], :realname => 'Net::YAIL', :username => 'Username'
     )
-    yail.prepend_handler(:incoming_welcome) { |text, args| @msg[:welcome] += 1 }
-    yail.prepend_handler(:incoming_endofmotd) {|text, args| @msg[:endofmotd] += 1 }
-    yail.start_listening
+  end
+
+  # Log in to fake server, give name, choose nick, join a channel, get kicked, quit
+  def test_simple
+    @yail.prepend_handler(:incoming_welcome)    { |text, args|                          @msg[:welcome] += 1 }
+    @yail.prepend_handler(:incoming_endofmotd)  { |text, args|                          @msg[:endofmotd] += 1 }
+    @yail.prepend_handler(:incoming_notice)     { |fullactor, actor, target, text|      @msg[:notice] += 1 }
+    @yail.start_listening
 
     # Give us a chance to catch all events
     until ( 1 == @msg[:endofmotd] )
